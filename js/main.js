8f688762ec7df84d1fe8adc0d44c156db363848d
zoduaks <zoduaks@gmail.com>
2021-07-23 01:26:43 +0300

    ariaHidden, конкатинация, звезды (рейтинг)

--- js/main.js
+++ js/main.js
@@ -56,6 +56,20 @@ const createHeader = (param) => {
 		wrapper.append(logo);
 	}
 
+	if(param.header.menu) {
+		const nav = getElement('nav', ['menu-list']);
+		const allMenuLink = param.header.menu.map(item => {
+			const link = getElement('a', ['menu-link'], {
+				href: item.link,
+				textContent: item.title,
+			})
+
+			return link;
+		})
+		nav.append(...allMenuLink);
+		wrapper.append(nav);
+	}
+
 	if(param.header.social) {
 		const socialWrapper = getElement('div', ['social']);
 		const allSocial = param.header.social.map(item => {
@@ -73,29 +87,118 @@ const createHeader = (param) => {
 		wrapper.append(socialWrapper);
 	}
 
-	if(param.header.menu) {
-
-	}
-
 	header.append(container);
 	container.append(wrapper);
 
 	return header;
 };
 
+
+const createMain = ({title, main: { genre, rating, description, trailer } }) => {
+	// const createMain = (param) => {
+	// 	const {title, main: { genre, rating, description, trailer } } = param;
+	//имя должно совпадать с ключем что у main 
+	// const { genre, rating, description, trailer } = main;
+
+	const main = getElement('main');
+	const container = getElement('div', ['container']);
+	main.append(container);
+	const wrapper = getElement('div', ['main-content']);
+	container.append(wrapper);
+	const content = getElement('div', ['content']);
+	wrapper.append(content);
+
+	if(genre) {
+		const genreSpan = getElement('span', 
+		['genre', 'animated', 'fadeInRight'],
+		{
+			textContent: genre
+		});
+
+		content.append(genreSpan);
+	}
+
+	if(rating) {
+		const ratingBlock = getElement('div', ['rating', 'animated', 'fadeInRight']);
+		const ratingStars = getElement('div', ['rating-stars']);
+		const ratingNumber = getElement('div', ['rating-number'], {
+			textContent: `${rating}/10`,
+		});
+
+		for (let i = 0; i < 10; i++) {
+			const star = getElement('img', ['star'], {
+				alt: i ? '' : `Рейтинг ${rating} из 10`,
+				src: i < rating ? 'img/star.svg' : 'img/star-o.svg' ,
+			})
+			ratingStars.append(star);
+		}
+
+		ratingBlock.append(ratingStars, ratingNumber);
+		content.append(ratingBlock);
+	}
+
+	content.append(getElement('h1', ['main-title', 'animated', 'fadeInRight'], {
+		textContent: title,
+	}))
+
+	if(description) {
+		content.append(getElement('p', 
+		['main-description', 'animated', 'fadeInRight'],
+		{textContent: description}
+		));
+	}
+
+	if(trailer) {
+		const youTubeLink = getElement('a', 
+		['button', 'animated', 'fadeInRight', 'youtube-modal'], 
+		{
+			href: trailer,
+			textContent: 'Смотреть трейлер',
+		})
+
+		const youTubeImgLink = getElement('a', ['play', 'youtube-modal'], {
+				href: trailer,
+				ariaLabel: 'Смотреть трейлер',
+		})
+
+		const iconPlay = getElement('img', ['play-img'], {
+			src: 'img/play.svg', 
+			alt: 'смотреть трейле',
+			// скрывать для слабовидящих 
+			ariaHidden: true,
+		})
+
+		content.append(youTubeLink);
+		youTubeImgLink.append(iconPlay);
+		wrapper.append(youTubeImgLink);
+	}
+
+	return main;
+}
+
 const movieConstructor = (selector, options) => {
 	
 	const app = document.querySelector(selector);
 	app.classList.add('body-app');
+
+	app.style.backgroundImage = options.background ? 
+		`url("${options.background}")` : '';
+
+	document.title = options.title;
 	
 	if(options.header){
-			app.append(createHeader(options));
+		app.append(createHeader(options));
+	}
+
+	if(options.main) {
+		app.append(createMain(options));
 	}
 
 
 };
 movieConstructor('.app', {
 	title: 'Ведьмак',
+	background: 'witcher/background.jpg',
 	header: {
 		logo: 'witcher/logo.png',
 		social: [
@@ -129,5 +232,11 @@ movieConstructor('.app', {
 				link: '#',
 			},
 		]
+	},
+	main: {
+		genre: '2019,фэнтези',
+		rating: 8,
+		description: 'Ведьмак Геральт, мутант и убийца чудовищ, на своей верной лошади по кличке Плотва путешествует по Континенту. За тугой мешочек чеканных монет этот мужчина избавит вас от всякой настырной нечисти — хоть от чудищ болотных, оборотней и даже заколдованных принцесс.',
+		trailer: 'https://www.youtube.com/watch?v=P0oJqfLzZzQ',
 	},
 });
